b55a17dba24e6123a81c2ddbac004fcd41a30686
Mathieu Boulet <mathieu.boulet.44@gmail.com>
2017-03-13 11:03:27 +0100

    add gui check box

---
+++ includes/Gui/GuiCheckBox.h
@@ -0,0 +1,31 @@
+#ifndef IMP_GUI_CHECKBOX_H
+#define IMP_GUI_CHECKBOX_H
+
+#include "Core/impBase.h"
+#include <Gui/GuiButton.h>
+#include <Gui/GuiText.h>
+
+IMPGEARS_BEGIN
+
+class IMP_API GuiCheckBox
+	: public GuiComponent
+{
+	public:
+	GuiCheckBox();
+	virtual ~GuiCheckBox();
+
+	virtual void renderComponent(imp::Uint32 passID, float parentX = 0.0, float parentY = 0.0);
+
+	void setChecked(bool checked){_checked = checked;}
+	bool isChecked() const {return _checked;}
+
+	private:
+	bool _checked;
+
+	GuiButton* check;
+	GuiText* rightComponent;
+};
+
+IMPGEARS_END
+
+#endif // IMP_GUI_CHECKBOX_H

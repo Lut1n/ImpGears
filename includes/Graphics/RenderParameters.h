81ece2c87fe7bb0d1bab0c631318ccaf56d7123c
Lut1n <mathieu.boulet.44@gmail.com>
2017-07-22 09:40:21 +0200

    add function apply() in RenderParameters (to be used with GraphicState)

--- includes/Graphics/RenderParameters.h
+++ includes/Graphics/RenderParameters.h
@@ -27,6 +27,12 @@ class IMP_API RenderParameters
             BlendMode_SrcAlphaBased
         };
 
+        enum ParamState
+        {
+            ParamState_Disable = 0,
+            ParamState_Enable
+        };
+
         RenderParameters();
         RenderParameters(const RenderParameters& other);
         virtual ~RenderParameters();
@@ -36,28 +42,43 @@ class IMP_API RenderParameters
         void setClearColor(const Vector3& clearColor);
         const Vector3& getClearColor() const{return m_clearColor;}
 
-        void setFaceCullingMode(FaceCullingMode faceCullingMode){m_faceCullingMode = faceCullingMode;}
+        void setFaceCullingMode(FaceCullingMode faceCullingMode){m_faceCullingMode = faceCullingMode;_faceCullingChanged=true;}
         FaceCullingMode getFaceCullingMode() const{return m_faceCullingMode;}
 
-        void setBlendMode(BlendMode blendMode){m_blendMode = blendMode;}
+        void setBlendMode(BlendMode blendMode){m_blendMode = blendMode;_blendModeChanged=true;}
         BlendMode getBlendMode() const{return m_blendMode;}
 
         void enable() const;
         void disable() const;
+		
+		void apply() const;
 
         void setPerspectiveProjection(float fovx, float ratio, float nearValue, float farValue);
         void setOrthographicProjection(float left, float right, float bottom, float top, float nearValue, float farValue);
 
         void setProjectionMatrix(const Matrix4& projection){m_projectionMatrix = projection;}
         const Matrix4& getProjectionMatrix() const {return m_projectionMatrix;}
+		
+		void setFog(ParamState state);
+		void setClearDepth(float depth);
+		
+		static RenderParameters* createDefaultParameters();
 
     protected:
     private:
 
         Vector3 m_clearColor;
+		float _clearDepth;
         Matrix4 m_projectionMatrix;
         FaceCullingMode m_faceCullingMode;
         BlendMode m_blendMode;
+		ParamState _fogState;
+		
+		bool _clearColorChanged;
+		bool _clearDepthChanged;
+		bool _faceCullingChanged;
+		bool _blendModeChanged;
+		bool _fogChanged;
 };
 
 IMPGEARS_END
--- src/Graphics/RenderParameters.cpp
+++ src/Graphics/RenderParameters.cpp
@@ -5,9 +5,16 @@ IMPGEARS_BEGIN
 //--------------------------------------------------------------
 RenderParameters::RenderParameters():
     m_faceCullingMode(FaceCullingMode_Back),
-    m_blendMode(BlendMode_SrcAlphaBased)
+    m_blendMode(BlendMode_SrcAlphaBased),
+	_clearColorChanged(false),
+	_clearDepthChanged(false),
+	_faceCullingChanged(false),
+	_blendModeChanged(false),
+	_fogChanged(false)
 {
     m_clearColor = Vector3(1.f, 1.f, 1.f); //alpha = 1.f
+	_clearDepth = 1.f;
+	_fogState = ParamState_Disable;
     m_projectionMatrix = Matrix4::getPerspectiveProjectionMat(45.f, 4.f/3.f, 0.1f, 100.f);
 }
 
@@ -18,6 +25,8 @@ RenderParameters::RenderParameters(const RenderParameters& other):
 {
     m_clearColor = other.m_clearColor;
     m_projectionMatrix = other.m_projectionMatrix;
+	_clearDepth = other._clearDepth;
+	_fogState = other._fogState;
 }
 
 //--------------------------------------------------------------
@@ -33,6 +42,8 @@ const RenderParameters& RenderParameters::operator=(const RenderParameters& othe
     m_blendMode = other.m_blendMode;
     m_clearColor = other.m_clearColor;
     m_projectionMatrix = other.m_projectionMatrix;
+	_clearDepth = other._clearDepth;
+	_fogState = other._fogState;
 
     return *this;
 }
@@ -41,6 +52,7 @@ const RenderParameters& RenderParameters::operator=(const RenderParameters& othe
 void RenderParameters::setClearColor(const Vector3& clearColor)
 {
     m_clearColor = clearColor;
+	_clearColorChanged = true;
 }
 
 //--------------------------------------------------------------
@@ -83,21 +95,80 @@ void RenderParameters::enable() const
 
     /// Fog
     glDisable(GL_FOG);
-    /*glEnable(GL_FOG);
-    GLfloat fogDensity = 2.f;
-    GLfloat fogColor[4] = {0.8f, 0.8f, 0.8f, 1.f};
-    glFogi(GL_FOG_MODE, GL_EXP2);
-    //glFogi(GL_FOG_MODE, GL_LINEAR);
-    glFogfv(GL_FOG_COLOR, fogColor);
-    glFogf(GL_FOG_DENSITY, fogDensity);
-    glFogf(GL_FOG_START, 1.f);
-    glFogf(GL_FOG_END, 100.f);
-    glHint(GL_FOG_HINT, GL_NICEST);*/
 }
 
 //--------------------------------------------------------------
 void RenderParameters::disable() const
 {
+}
+
+//--------------------------------------------------------------
+void RenderParameters::apply() const
+{
+	if(_clearColorChanged)
+	{
+		glClearColor(m_clearColor.getX(), m_clearColor.getY(), m_clearColor.getZ(), 0.f);
+	}
+	
+	if(_faceCullingChanged)
+	{
+		if(m_faceCullingMode == FaceCullingMode_None)
+		{
+			glDisable(GL_CULL_FACE);
+		}
+		else
+		{
+			glEnable(GL_CULL_FACE);
+			glFrontFace(GL_CCW); // GL_CW or GL_CCW
+
+			if(m_faceCullingMode == FaceCullingMode_Back)
+				glCullFace(GL_BACK); // GL_FRONT, GL_BACK or GL_FRONT_AND_BACK
+			else if(m_faceCullingMode == FaceCullingMode_Front)
+				glCullFace(GL_FRONT);
+		}
+	}
+	
+	if(_blendModeChanged)
+	{
+		if(m_blendMode == BlendMode_None)
+		{
+			glDisable(GL_BLEND);
+		}
+		else if(m_blendMode == BlendMode_SrcAlphaBased)
+		{
+			glEnable(GL_BLEND);
+			glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+		}
+	}
+	
+	if(_clearDepthChanged)
+	{
+		glEnable(GL_DEPTH_TEST);
+		glDepthFunc(GL_LEQUAL);
+		glDepthMask(GL_TRUE);
+		glClearDepth(_clearDepth);
+	}
+	
+	if(_fogChanged)
+	{
+		if(_fogState == ParamState_Disable)
+		{
+			glDisable(GL_FOG);
+		}
+		else
+		{
+			glEnable(GL_FOG);
+			GLfloat fogColor[4] = {0.8f, 0.8f, 0.8f, 1.f};
+			glFogfv(GL_FOG_COLOR, fogColor);
+			glFogi(GL_FOG_MODE, GL_EXP2);
+			//glFogi(GL_FOG_MODE, GL_LINEAR);
+			GLfloat fogDensity = 2.f;
+			glFogf(GL_FOG_DENSITY, fogDensity);
+			glFogf(GL_FOG_START, 1.f);
+			glFogf(GL_FOG_END, 100.f);
+			glHint(GL_FOG_HINT, GL_NICEST);
+		}
+	}
 }
 
 //--------------------------------------------------------------
@@ -110,6 +181,34 @@ void RenderParameters::setPerspectiveProjection(float fovx, float ratio, float n
 void RenderParameters::setOrthographicProjection(float left, float right, float bottom, float top, float nearValue, float farValue)
 {
     m_projectionMatrix = Matrix4::getOrthographicProjectionMat(left, right, bottom, top, nearValue, farValue);
+}
+		
+//--------------------------------------------------------------
+void RenderParameters::setFog(ParamState state)
+{
+	_fogState = state;
+	_fogChanged = true;
+}
+
+//--------------------------------------------------------------
+void RenderParameters::setClearDepth(float depth)
+{
+	_clearDepth = depth;
+	_clearDepthChanged = true;
+}
+
+//--------------------------------------------------------------
+RenderParameters* RenderParameters::createDefaultParameters()
+{
+	RenderParameters* params = new RenderParameters();
+	
+	params->setClearColor( Vector3(1.f, 1.f, 1.f) );
+	params->setClearDepth( 1.f );
+	params->setFog( ParamState_Disable );
+	params->setBlendMode(BlendMode_SrcAlphaBased);
+	params->setFaceCullingMode(FaceCullingMode_Back);
+	
+	return params;
 }
 
 IMPGEARS_END

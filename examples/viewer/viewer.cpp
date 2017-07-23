b0b157077e0e7d4523af7dcdbc60d0dd15434d6b
Lut1n <mathieu.boulet.44@gmail.com>
2017-07-23 18:58:25 +0200

    update viewer example

--- examples/viewer/viewer.cpp
+++ examples/viewer/viewer.cpp
@@ -1,6 +1,7 @@
 #include "Graphics/BmpLoader.h"
 #include "Graphics/DefaultShader.h"
 #include "Graphics/GraphicRenderer.h"
+#include "Graphics/GraphicState.h"
 #include "Graphics/RenderTarget.h"
 
 #include "Core/Timer.h"
@@ -11,92 +12,141 @@
 
 #include <System/FileInfo.h>
 
+imp::Texture* texture;
 
-void onEvent(imp::EvnContextInterface& evnContext)
+struct ViewerState
 {
+	imp::State state;
+	std::shared_ptr<imp::GraphicRenderer> renderer;
+	std::shared_ptr<imp::RenderParameters> screenParameters;
+	std::shared_ptr<imp::RenderTarget> screenTarget;
+
+	std::shared_ptr<imp::DefaultShader> defaultShader;
+	imp::ScreenVertex* screen;
+
+	imp::EvnContextInterface* evnContext;
+
+	time_t accessLast, modifLast, statusLast;
+
+	std::string filename;
+	
+	ViewerState()
+	{	
+		evnContext = new imp::SFMLContextInterface();
+		evnContext->createWindow(800,600);
+		evnContext->setCursorVisible(0, true);
+		
+		// screen render target
+		screenTarget = std::shared_ptr<imp::RenderTarget>( new imp::RenderTarget() );
+		screenTarget->createScreenTarget(0);
+		
+		renderer = std::shared_ptr<imp::GraphicRenderer>(new imp::GraphicRenderer(0,IMP_NULL) );
+		renderer->setCenterCursor(false);
+	
+		// screen render parameters
+		screenParameters = std::shared_ptr<imp::RenderParameters>(new imp::RenderParameters() );
+		screenParameters->setOrthographicProjection(0.f, 1.f, 0.f, 1.f, 0.f, 1.f);
+		screenParameters->setClearColor(imp::Vector3(0.f, 0.f, 1.f));
+		
+		defaultShader = std::shared_ptr<imp::DefaultShader>(new imp::DefaultShader() );
+
+		const imp::Matrix4& i4 = imp::Matrix4::getIdentityMat();
+		defaultShader->addProjection( &(screenParameters->getProjectionMatrix() ));
+		defaultShader->addView(&i4);
+		defaultShader->addModel(&i4);
+		// defaultShader->addTextureParameter("u_colorTexture", texture, 0);
+	
+	
+		screen = new imp::ScreenVertex();
+		screen->getGraphicState()->setTarget(screenTarget);
+		screen->getGraphicState()->setParameters(screenParameters);
+		screen->getGraphicState()->setShader(defaultShader);
+		
+		renderer->setSceneRoot(screen);
+	}
+
+};
+
+void onEvent_private( imp::EvnContextInterface& evnContext )
+{
+	// get event
 	evnContext.getEvents(0);
 
+	// manage event
 	imp::Event event;
 	while (evnContext.nextEvent(event))
 		imp::State::getInstance()->onEvent(event);
+}
 
+void onEvent(imp::EvnContextInterface& evnContext)
+{
+	// engine event handling
+	onEvent_private(evnContext);
+
+	// user event handling
 	imp::State* state = imp::State::getInstance();
 
 	if(state->m_pressedKeys[imp::Event::Escape])
 		exit(0);
+
+}
+
+void onUpdate(ViewerState& state)
+{
+	// reload if changed
+	time_t access, modif, status;
+	imp::getTimeInfo(const_cast<char*>(state.filename.c_str()), access, modif, status);
+
+	if(modif > state.modifLast)
+	{
+		state.modifLast = modif;
+		texture = BmpLoader::loadFromFile( state.filename.c_str() );
+		
+		state.state.setWindowDim(texture->getWidth(), texture->getHeight());
+		state.evnContext->resizeWindow(0, texture->getWidth(), texture->getHeight());
+		state.evnContext->setWindowTitle(0, state.filename.c_str());
+	}
+}
+
+void onDraw(ViewerState& state)
+{
 }
 
 int main(int argc, char* argv[])
 {
-	imp::Timer fpsTimer;
-	imp::State state;
-
-	imp::EvnContextInterface* evnContext = new imp::SFMLContextInterface();
-	evnContext->createWindow(800,600);
-	evnContext->setCursorVisible(0, true);
-
-	imp::GraphicRenderer renderer(0, NULL);
-	renderer.setCenterCursor(false);
-
+	texture = IMP_NULL;
+	
+	ViewerState v_state;
+	
 	if(argc<2)
-		return 0;
-
-	imp::Texture* texture = BmpLoader::loadFromFile(argv[1]);
-	if(texture == IMP_NULL)
-		return 0;
-
-
-	time_t accessLast, modifLast, statusLast;
-	imp::getTimeInfo(argv[1], accessLast, modifLast, statusLast);
-
-	state.setWindowDim(texture->getWidth(), texture->getHeight());
-	//state.setWindowDim(800, 600);
-
-	 evnContext->resizeWindow(0, texture->getWidth(), texture->getHeight());
-	evnContext->setWindowTitle(0, argv[1]);
-
-	// screen render parameters
-	imp::RenderParameters screenParameters;
-	screenParameters.setOrthographicProjection(0.f, 1.f, 0.f, 1.f, 0.f, 1.f);
-	screenParameters.setClearColor(imp::Vector3(0.f, 0.f, 1.f));
-
-	// screen render target
-	imp::RenderTarget screenTarget;
-	screenTarget.createScreenTarget(0);
-
-	imp::Matrix4 i4 = imp::Matrix4::getIdentityMat();
-
-	imp::DefaultShader defaultShader;
-	imp::ScreenVertex screen;
-
-	while (evnContext->isOpen(0))
 	{
+		std::cout << "error : no file path given" << std::endl;
+		return 0;
+	}
+
+	v_state.filename = argv[1];
+	texture = BmpLoader::loadFromFile(v_state.filename.c_str());
+	if(texture == IMP_NULL)
+	{
+		std::cout << "error : BMP file loading has failed" << std::endl;
+		return 0;
+	}
+
+	v_state.defaultShader->addTextureParameter("u_colorTexture", texture, 0);
+	imp::getTimeInfo(const_cast<char*>(v_state.filename.c_str()), v_state.accessLast, v_state.modifLast, v_state.statusLast);
+	
+	while (v_state.evnContext->isOpen(0))
+	{
+	
 		onEvent(*imp::EvnContextInterface::getInstance());
+	
+		onUpdate(v_state);
+	
+		// rendering
+		v_state.renderer->renderScene(-1);
 
-		// reload if changed
-		time_t access, modif, status;
-		imp::getTimeInfo(argv[1], access, modif, status);
-		// std::cout << "MODIF time " << modif << "; " << modifLast << "\n";
-		if(modif > modifLast)
-		{
-			modifLast = modif;
-			texture = BmpLoader::loadFromFile(argv[1]);
-		}
-
-		renderer.setCamera(IMP_NULL);
-		renderer.setRenderParameters(screenParameters);
-		screenTarget.bind();
-		defaultShader.enable();
-		defaultShader.setMatrix4Parameter("u_projection", renderer.getProjectionMatrix());
-		defaultShader.setMatrix4Parameter("u_view", i4);
-		defaultShader.setMatrix4Parameter("u_model", i4);
-		defaultShader.setTextureParameter("u_colorTexture", texture, 0);
-		renderer.renderScene(-1);
-		screen.render(0);
-		defaultShader.disable();
-		screenTarget.unbind();
-
-		evnContext->display(0);
+		// display
+		v_state.evnContext->display(0);
 	}
 
 	delete imp::VBOManager::getInstance();

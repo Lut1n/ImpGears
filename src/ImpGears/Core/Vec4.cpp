91b6ae0695c27dcbb1f2e63ee319d4dbefb21ecc
Mathieu Boulet <mathieu.boulet.44@gmail.com>
2019-03-29 10:47:12 +0100

    with previous commit (Vec4.cpp)

---
+++ src/ImpGears/Core/Vec4.cpp
@@ -0,0 +1,10 @@
+#include <Core/Vec4.h>
+
+IMPGEARS_BEGIN
+
+const Vec4 Vec4::X(1.0,0.0,0.0,0.0);
+const Vec4 Vec4::Y(0.0,1.0,0.0,0.0);
+const Vec4 Vec4::Z(0.0,0.0,1.0,0.0);
+const Vec4 Vec4::W(0.0,0.0,0.0,1.0);
+
+IMPGEARS_END

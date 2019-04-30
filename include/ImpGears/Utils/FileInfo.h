e05c83bb115ceafbbb3dc319a8f5df7581c7e80f
Mathieu Boulet <mathieu.boulet.44@gmail.com>
2019-04-30 23:40:02 +0200

    add function for file operations

--- include/ImpGears/Utils/FileInfo.h
+++ include/ImpGears/Utils/FileInfo.h
@@ -3,26 +3,16 @@
 
 #include <Core/Object.h>
 
-// struct stat is defined in <sys/stat.h>
-#include <sys/stat.h>
-// time_t is defined in <sys/types.h>
-
-//#include <time.h>
-
 IMPGEARS_BEGIN
 
-void IMP_API getTimeInfo(char *filename, time_t& access, time_t& modif, time_t& status)
-{
-	if(filename)
-	{
-		struct stat t_stat;
-		stat(filename, &t_stat);
+//-------------------------------------------------------
+std::string IMP_API loadText(const std::string& filename);
 
-		access = t_stat.st_atime;	//time of last access
-		modif = t_stat.st_mtime;	//time of last data modification
-		status = t_stat.st_ctime;	//time of last status change
-	}
-}
+//-------------------------------------------------------
+bool IMP_API fileExists(const std::string& filename);
+
+//-------------------------------------------------------
+void IMP_API getTimeInfo(const std::string& filename, long& access, long& modif, long& status);
 
 IMPGEARS_END
 
---
+++ src/ImpGears/Utils/FileInfo.cpp
@@ -0,0 +1,40 @@
+#include <Utils/FileInfo.h>
+
+// struct stat is defined in <sys/stat.h>
+#include <sys/stat.h>
+// time_t is defined in <sys/types.h>
+
+//#include <time.h>
+
+#include <fstream>
+
+IMPGEARS_BEGIN
+
+//-------------------------------------------------------
+std::string loadText(const std::string& filename)
+{
+	std::ifstream ifs(filename);
+	std::string buffer( (std::istreambuf_iterator<char>(ifs) ),
+						(std::istreambuf_iterator<char>()    ) );
+	return buffer;
+}
+
+//-------------------------------------------------------
+bool fileExists(const std::string& filename)
+{
+	std::ifstream f(filename.c_str());
+	return f.good();
+}
+
+//-------------------------------------------------------
+void getTimeInfo(const std::string& filename, long& access, long& modif, long& status)
+{
+	struct stat t_stat;
+	stat(filename.c_str(), &t_stat);
+
+	access = (long)t_stat.st_atime; //time of last access
+	modif = (long)t_stat.st_mtime; //time of last data modification
+	status = (long)t_stat.st_ctime; //time of last status change
+}
+
+IMPGEARS_END

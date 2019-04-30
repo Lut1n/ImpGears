cdea3b51d620343def3a122a9177529044dceb3f
Mathieu Boulet <mathieu.boulet.44@gmail.com>
2019-04-30 20:46:08 +0200

    add Vec2

---
+++ include/ImpGears/Core/Vec2.h
@@ -0,0 +1,41 @@
+#ifndef IMP_VEC2_H
+#define IMP_VEC2_H
+
+#include <Core/Vec.h>
+
+#include <cmath>
+
+IMPGEARS_BEGIN
+
+class IMP_API Vec2 : public Vec<2,float>
+{
+	public:
+	
+	Meta_Class(Vec2)
+	
+	static const Vec2 X;
+	static const Vec2 Y;
+	
+	Vec2() : Vec() {}
+	Vec2(float v) : Vec(v) {}
+	Vec2(float x, float y) : Vec(x,y) {}
+	Vec2(const float* buf) : Vec(buf) {}
+	
+	template<int Dim2>
+	Vec2(const Vec<Dim2,float>& v, float dv=1) : Vec<2,float>(v,dv) {}
+	
+	template<int Dim2>
+	const Vec2& operator=(const Vec<Dim2,float>& other) { Vec<2,float>::operator=(other); return *this; }
+	
+	void set(float x, float y) { Vec<2,float>::set(x,y); }
+	
+	float x() const { return _data[0]; }
+	float y() const { return _data[1]; }
+	
+	float& x() { return _data[0]; }
+	float& y() { return _data[1]; }
+};
+
+IMPGEARS_END
+
+#endif // IMP_VEC2_H
---
+++ src/ImpGears/Core/Vec2.cpp
@@ -0,0 +1,8 @@
+#include <Core/Vec2.h>
+
+IMPGEARS_BEGIN
+
+const Vec2 Vec2::X(1.0,0.0);
+const Vec2 Vec2::Y(0.0,1.0);
+
+IMPGEARS_END

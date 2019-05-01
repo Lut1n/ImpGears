3e7ee976393ccb1d10fead5f2c04043a015f8d08
Mathieu Boulet <mathieu.boulet.44@gmail.com>
2019-05-01 20:01:21 +0200

    clean Matrix implementation

--- src/ImpGears/Core/Matrix3.cpp
+++ src/ImpGears/Core/Matrix3.cpp
@@ -7,18 +7,19 @@
 
 IMPGEARS_BEGIN
 
-#define IDX(x,y)_data[y*3+x]
-
+//--------------------------------------------------------------
 Matrix3::Matrix3()
 	: Matrix<3,3,float>()
 {
 }
 
+//--------------------------------------------------------------
 Matrix3::Matrix3(const float* buf, bool transp)
 	: Matrix<3,3,float>(buf,transp)
 {
 }
 
+//--------------------------------------------------------------
 Matrix3::Matrix3(const Vec3& c1,const Vec3& c2,const Vec3& c3)
 {
 	at(0,0)=c1[0]; at(0,1)=c1[1]; at(0,2)=c1[2];
@@ -26,132 +27,98 @@ Matrix3::Matrix3(const Vec3& c1,const Vec3& c2,const Vec3& c3)
 	at(2,0)=c3[0]; at(2,1)=c3[1]; at(2,2)=c3[2];
 }
 
+//--------------------------------------------------------------
 Matrix3::~Matrix3()
 {
 }
 
+//--------------------------------------------------------------
 Matrix3& Matrix3::operator+=(const Matrix3& other)
 {
-    for(std::uint32_t i=0; i<9; ++i)
-        _data[i] += other._data[i];
-
+    for(int i=0; i<9; ++i) _data[i] += other._data[i];
     return *this;
 }
 
+//--------------------------------------------------------------
 Matrix3& Matrix3::operator-=(const Matrix3& other)
 {
-    for(std::uint32_t i=0; i<9; ++i)
-        _data[i] -= other._data[i];
-
-    return *this;
+	for(int i=0; i<9; ++i) _data[i] -= other._data[i];
+	return *this;
 }
 
+//--------------------------------------------------------------
 Matrix3 Matrix3::operator+(const Matrix3& other) const
 {
-    return Matrix3(*this) += other;
+	return Matrix3(*this) += other;
 }
 
+//--------------------------------------------------------------
 Matrix3 Matrix3::operator-(const Matrix3& other) const
 {
-    return Matrix3(*this) -= other;
+	return Matrix3(*this) -= other;
 }
 
+//--------------------------------------------------------------
 float Matrix3::det() const
 {
-    /**
-    A = | a b c |
-        | d e f |
-        | g h i |
+	float result =
+	at(0,0)*at(1,1)*at(2,2)
+	+ at(1,0)*at(2,1)*at(0,2)
+	+ at(2,0)*at(0,1)*at(1,2)
+	- at(2,0)*at(1,1)*at(0,2)
+	- at(2,1)*at(1,2)*at(0,0)
+	- at(2,2)*at(1,0)*at(0,1);
 
-    det(A) = aei + bfg + cdh - ceg - fha - ibd
-    */
-
-    float result =
-    IDX(0,0)*IDX(1,1)*IDX(2,2)
-    + IDX(1,0)*IDX(2,1)*IDX(0,2)
-    + IDX(2,0)*IDX(0,1)*IDX(1,2)
-    - IDX(2,0)*IDX(1,1)*IDX(0,2)
-    - IDX(2,1)*IDX(1,2)*IDX(0,0)
-    - IDX(2,2)*IDX(1,0)*IDX(0,1);
-
-    return result;
+	return result;
 }
 
+//--------------------------------------------------------------
 Matrix3 Matrix3::inverse() const
 {
-    /**
-    A = | a b c |
-        | d e f |
-        | g h i |
-
-    A-1 = 1/det(A) * transpose( | ei-fh  fg-di  dh-eg | )
-                                | ch-bi  ai-cg  bg-ah |
-                                | bf-ce  cd-af  ae-bd |
-    */
-
-    float result[9];
-    result[0] = IDX(1,1)*IDX(2,2) - IDX(2,1)*IDX(1,2);
-    result[1] = IDX(2,0)*IDX(1,2) - IDX(1,0)*IDX(2,2);
-    result[2] = IDX(1,0)*IDX(2,1) - IDX(2,0)*IDX(1,1);
-
-    result[3] = IDX(2,1)*IDX(0,2) - IDX(0,1)*IDX(2,2);
-    result[4] = IDX(0,0)*IDX(2,2) - IDX(2,0)*IDX(0,2);
-    result[5] = IDX(2,0)*IDX(0,1) - IDX(0,0)*IDX(2,1);
-
-    result[6] = IDX(0,1)*IDX(1,2) - IDX(1,1)*IDX(0,2);
-    result[7] = IDX(1,0)*IDX(0,2) - IDX(0,0)*IDX(1,2);
-    result[8] = IDX(0,0)*IDX(1,1) - IDX(1,0)*IDX(0,1);
-
-    Matrix3 mat(result);
-    float matDet = mat.det();
-    mat.transpose();
-
-    return mat * (1.f/matDet);
+	Matrix3 mat;
+	mat(0,0) = at(1,1)*at(2,2) - at(2,1)*at(1,2);
+	mat(0,1) = at(2,0)*at(1,2) - at(1,0)*at(2,2);
+	mat(0,2) = at(1,0)*at(2,1) - at(2,0)*at(1,1);
+	
+	mat(1,0) = at(2,1)*at(0,2) - at(0,1)*at(2,2);
+	mat(1,1) = at(0,0)*at(2,2) - at(2,0)*at(0,2);
+	mat(1,2) = at(2,0)*at(0,1) - at(0,0)*at(2,1);
+	
+	mat(2,0) = at(0,1)*at(1,2) - at(1,1)*at(0,2);
+	mat(2,1) = at(1,0)*at(0,2) - at(0,0)*at(1,2);
+	mat(2,2) = at(0,0)*at(1,1) - at(1,0)*at(0,1);
+	
+	return mat.transpose() * (1.f/det());
 }
 
+//--------------------------------------------------------------
 Matrix3 Matrix3::rotationX(float rad)
 {
-    ///     1           0           0
-    ///     0           std::cos(a)      -std::sin(a)
-    ///     0           std::sin(a)      std::cos(a)
-
-    const float values[9] = {
-        1.f,        0.f,            0.f,
-        0.f,        std::cos(rad),      -std::sin(rad),
-        0.f,        std::sin(rad),      std::cos(rad)
-    };
-
-    return Matrix3(values,true);
+	float c = std::cos(rad);
+	float s = std::sin(rad);
+	
+	const float values[9] = { 1.f,0.f,0.f,0.f,c,-s,0.f,s,c};
+	return Matrix3(values,true);
 }
 
+//--------------------------------------------------------------
 Matrix3 Matrix3::rotationY(float rad)
 {
-    ///     std::cos(a)      0           std::sin(a)
-    ///     0           1           0
-    ///     -std::sin(a)     0           std::cos(a)
-
-    const float values[9] = {
-        std::cos(rad),      0.f,        std::sin(rad),
-        0.f,            1.f,        0.f,
-        -std::sin(rad),     0.f,        std::cos(rad)
-    };
-
-    return Matrix3(values,true);
+	float c = std::cos(rad);
+	float s = std::sin(rad);
+	
+	const float values[9] = { c,0.f,s,0.f,1.f,0.f,-s,0.f,c};
+	return Matrix3(values,true);
 }
 
+//--------------------------------------------------------------
 Matrix3 Matrix3::rotationZ(float rad)
 {
-    ///     std::cos(a)      -std::sin(a)     0
-    ///     std::sin(a)      std::cos(a)      0
-    ///     0           0           1
-
-    const float values[9] = {
-        std::cos(rad),      -std::sin(rad),         0.f,
-        std::sin(rad),      std::cos(rad),          0.f,
-        0.f,            0.f,                1.f
-    };
-
-    return Matrix3(values,true);
+	float c = std::cos(rad);
+	float s = std::sin(rad);
+	
+	const float values[9] = { c,-s,0.f,s,c,0.f,0.f,0.f,1.f};
+	return Matrix3(values,true);
 }
 
 //--------------------------------------------------------------
@@ -163,13 +130,14 @@ Matrix3 Matrix3::rotationXYZ(const Vec3& r)
 //--------------------------------------------------------------
 Matrix3 Matrix3::scale(float sx, float sy, float sz)
 {
-    const float data[9] = {
-        sx,     0.f,    0.f,
-        0.f,    sy,     0.f,
-        0.f,    0.f,    sz
-    };
+	const float data[9] =
+	{
+		sx,     0.f,    0.f,
+		0.f,    sy,     0.f,
+		0.f,    0.f,    sz
+	};
 
-    return Matrix3(data);
+	return Matrix3(data);
 }
 
 //--------------------------------------------------------------
--- src/ImpGears/Core/Matrix4.cpp
+++ src/ImpGears/Core/Matrix4.cpp
@@ -5,8 +5,6 @@
 
 IMPGEARS_BEGIN
 
-#define IDX(x,y)_data[(y-1)*4+(x-1)]
-
 //--------------------------------------------------------------
 Matrix4::Matrix4()
 	: Matrix<4,4,float>()
@@ -36,206 +34,213 @@ Matrix4::~Matrix4()
 //--------------------------------------------------------------
 float Matrix4::det() const
 {
-    float result = IDX(1,1)*IDX(2,2)*IDX(3,3)*IDX(4,4) + IDX(1,1)*IDX(2,3)*IDX(3,4)*IDX(4,2) + IDX(1,1)*IDX(2,4)*IDX(3,2)*IDX(4,3)
-                 + IDX(1,2)*IDX(2,1)*IDX(3,4)*IDX(4,3) + IDX(1,2)*IDX(2,3)*IDX(3,1)*IDX(4,4) + IDX(1,2)*IDX(2,4)*IDX(3,3)*IDX(4,1)
-                 + IDX(1,3)*IDX(2,1)*IDX(3,2)*IDX(4,4) + IDX(1,3)*IDX(2,2)*IDX(3,4)*IDX(4,1) + IDX(1,3)*IDX(2,4)*IDX(3,1)*IDX(4,2)
-                 + IDX(1,4)*IDX(2,1)*IDX(3,3)*IDX(4,2) + IDX(1,4)*IDX(2,2)*IDX(3,1)*IDX(4,3) + IDX(1,4)*IDX(2,3)*IDX(3,2)*IDX(4,1)
-                 - IDX(1,1)*IDX(2,2)*IDX(3,4)*IDX(4,3) - IDX(1,1)*IDX(2,3)*IDX(3,2)*IDX(4,4) - IDX(1,1)*IDX(2,4)*IDX(3,3)*IDX(4,2)
-                 - IDX(1,2)*IDX(2,1)*IDX(3,3)*IDX(4,4) - IDX(1,2)*IDX(2,3)*IDX(3,4)*IDX(4,1) - IDX(1,2)*IDX(2,4)*IDX(3,1)*IDX(4,3)
-                 - IDX(1,3)*IDX(2,1)*IDX(3,4)*IDX(4,2) - IDX(1,3)*IDX(2,2)*IDX(3,1)*IDX(4,4) - IDX(1,3)*IDX(2,4)*IDX(3,2)*IDX(4,1)
-                 - IDX(1,4)*IDX(2,1)*IDX(3,2)*IDX(4,3) - IDX(1,4)*IDX(2,2)*IDX(3,3)*IDX(4,1) - IDX(1,4)*IDX(2,3)*IDX(3,1)*IDX(4,2);
+	float result = 
+	at(0,0)*at(1,1)*at(2,2)*at(3,3) + at(0,0)*at(2,1)*at(3,2)*at(1,3) + at(0,0)*at(3,1)*at(1,2)*at(2,3)
+	+ at(1,0)*at(0,1)*at(3,2)*at(2,3) + at(1,0)*at(2,1)*at(0,2)*at(3,3) + at(1,0)*at(3,1)*at(2,2)*at(0,3)
+	+ at(2,0)*at(0,1)*at(1,2)*at(3,3) + at(2,0)*at(1,1)*at(3,2)*at(0,3) + at(2,0)*at(3,1)*at(0,2)*at(1,3)
+	+ at(3,0)*at(0,1)*at(2,2)*at(1,3) + at(3,0)*at(1,1)*at(0,2)*at(2,3) + at(3,0)*at(2,1)*at(1,2)*at(0,3)
+	- at(0,0)*at(1,1)*at(3,2)*at(2,3) - at(0,0)*at(2,1)*at(1,2)*at(3,3) - at(0,0)*at(3,1)*at(2,2)*at(1,3)
+	- at(1,0)*at(0,1)*at(2,2)*at(3,3) - at(1,0)*at(2,1)*at(3,2)*at(0,3) - at(1,0)*at(3,1)*at(0,2)*at(2,3)
+	- at(2,0)*at(0,1)*at(3,2)*at(1,3) - at(2,0)*at(1,1)*at(0,2)*at(3,3) - at(2,0)*at(3,1)*at(1,2)*at(0,3)
+	- at(3,0)*at(0,1)*at(1,2)*at(2,3) - at(3,0)*at(1,1)*at(2,2)*at(0,3) - at(3,0)*at(2,1)*at(0,2)*at(1,3);
 
-    return result;
+	return result;
 }
 
 //--------------------------------------------------------------
 Matrix4 Matrix4::inverse() const
 {
-    float result[16];
+	Matrix4 mat;
 
-    result[0] = IDX(2,2)*IDX(3,3)*IDX(4,4) + IDX(2,3)*IDX(3,4)*IDX(4,2) + IDX(2,4)*IDX(3,2)*IDX(4,3)
-              - IDX(2,2)*IDX(3,4)*IDX(4,3) - IDX(2,3)*IDX(3,2)*IDX(4,4) - IDX(2,4)*IDX(3,3)*IDX(4,2);
-    result[1] = IDX(2,1)*IDX(3,4)*IDX(4,3) + IDX(2,3)*IDX(3,1)*IDX(4,4) + IDX(2,4)*IDX(3,3)*IDX(4,1)
-              - IDX(2,1)*IDX(3,3)*IDX(4,4) - IDX(2,3)*IDX(3,4)*IDX(4,1) - IDX(2,4)*IDX(3,1)*IDX(4,3);
-    result[2] = IDX(2,1)*IDX(3,2)*IDX(4,4) + IDX(2,2)*IDX(3,4)*IDX(4,1) + IDX(2,4)*IDX(3,1)*IDX(4,2)
-              - IDX(2,1)*IDX(3,4)*IDX(4,2) - IDX(2,2)*IDX(3,1)*IDX(4,4) - IDX(2,4)*IDX(3,2)*IDX(4,1);
-    result[3] = IDX(2,1)*IDX(3,3)*IDX(4,2) + IDX(2,2)*IDX(3,1)*IDX(4,3) + IDX(2,3)*IDX(3,2)*IDX(4,1)
-              - IDX(2,1)*IDX(3,2)*IDX(4,3) - IDX(2,2)*IDX(3,3)*IDX(4,1) - IDX(2,3)*IDX(3,1)*IDX(4,2);
+	mat(0,0) = at(1,1)*at(2,2)*at(3,3) + at(2,1)*at(3,2)*at(1,3) + at(3,1)*at(1,2)*at(2,3)
+				- at(1,1)*at(3,2)*at(2,3) - at(2,1)*at(1,2)*at(3,3) - at(3,1)*at(2,2)*at(1,3);
+	mat(0,1) = at(1,0)*at(3,2)*at(2,3) + at(2,0)*at(1,2)*at(3,3) + at(3,0)*at(2,2)*at(1,3)
+				- at(1,0)*at(2,2)*at(3,3) - at(2,0)*at(3,2)*at(1,3) - at(3,0)*at(1,2)*at(2,3);
+	mat(0,2) = at(1,0)*at(2,1)*at(3,3) + at(2,0)*at(3,1)*at(1,3) + at(3,0)*at(1,1)*at(2,3)
+				- at(1,0)*at(3,1)*at(2,3) - at(2,0)*at(1,1)*at(3,3) - at(3,0)*at(2,1)*at(1,3);
+	mat(0,3) = at(1,0)*at(3,1)*at(2,2) + at(2,0)*at(1,1)*at(3,2) + at(3,0)*at(2,1)*at(1,2)
+				- at(1,0)*at(2,1)*at(3,2) - at(2,0)*at(3,1)*at(1,2) - at(3,0)*at(1,1)*at(2,2);
+				
+	mat(1,0) = at(0,1)*at(3,2)*at(2,3) + at(2,1)*at(0,2)*at(3,3) + at(3,1)*at(2,2)*at(0,3)
+				- at(0,1)*at(2,2)*at(3,3) - at(2,1)*at(3,2)*at(0,3) - at(3,1)*at(0,2)*at(2,3);
+	mat(1,1) = at(0,0)*at(2,2)*at(3,3) + at(2,0)*at(3,2)*at(0,3) + at(3,0)*at(0,2)*at(2,3)
+				- at(0,0)*at(3,2)*at(2,3) - at(2,0)*at(0,2)*at(3,3) - at(3,0)*at(2,2)*at(0,3);
+	mat(1,2) = at(0,0)*at(3,1)*at(2,3) + at(2,0)*at(0,1)*at(3,3) + at(3,0)*at(2,1)*at(0,3)
+				- at(0,0)*at(2,1)*at(3,3) - at(2,0)*at(3,1)*at(0,3) - at(3,0)*at(0,1)*at(2,3);
+	mat(1,3) = at(0,0)*at(2,1)*at(3,2) + at(2,0)*at(3,1)*at(0,2) + at(3,0)*at(0,1)*at(2,2)
+				- at(0,0)*at(3,1)*at(2,2) - at(2,0)*at(0,1)*at(3,2) - at(3,0)*at(2,1)*at(0,2);
+				
+	mat(2,0) = at(0,1)*at(1,2)*at(3,3) + at(1,1)*at(3,2)*at(0,3) + at(3,1)*at(0,2)*at(1,3)
+				- at(0,1)*at(3,2)*at(1,3) - at(1,1)*at(0,2)*at(3,3) - at(3,1)*at(1,2)*at(0,3);
+	mat(2,1) = at(0,0)*at(3,2)*at(1,3) + at(1,0)*at(0,2)*at(3,3) + at(3,0)*at(1,2)*at(0,3)
+				- at(0,0)*at(1,2)*at(3,3) - at(1,0)*at(3,2)*at(0,3) - at(3,0)*at(0,2)*at(1,3);
+	mat(2,2) = at(0,0)*at(1,1)*at(3,3) + at(1,0)*at(3,1)*at(0,3) + at(3,0)*at(0,1)*at(1,3)
+				- at(0,0)*at(3,1)*at(1,3) - at(1,0)*at(0,1)*at(3,3) - at(3,0)*at(1,1)*at(0,3);
+	mat(2,3) = at(0,0)*at(3,1)*at(1,2) + at(1,0)*at(0,1)*at(3,2) + at(3,0)*at(1,1)*at(0,2)
+				- at(0,0)*at(1,1)*at(3,2) - at(1,0)*at(3,1)*at(0,2) - at(3,0)*at(0,1)*at(1,2);
+				
+	mat(3,0) = at(0,1)*at(2,2)*at(1,3) + at(1,1)*at(0,2)*at(2,3) + at(2,1)*at(1,2)*at(0,3)
+				- at(0,1)*at(1,2)*at(2,3) - at(1,1)*at(2,2)*at(0,3) - at(2,1)*at(0,2)*at(1,3);
+	mat(3,1) = at(0,0)*at(1,2)*at(2,3) + at(1,0)*at(2,2)*at(0,3) + at(2,0)*at(0,2)*at(1,3)
+				- at(0,0)*at(1,1)*at(2,3) - at(1,0)*at(2,1)*at(0,3) - at(2,0)*at(0,1)*at(1,3);
+	mat(3,2) = at(0,0)*at(2,1)*at(1,3) + at(1,0)*at(0,1)*at(2,3) + at(2,0)*at(1,1)*at(0,3)
+				- at(0,0)*at(1,1)*at(2,3) - at(1,0)*at(2,1)*at(0,3) - at(2,0)*at(0,1)*at(1,3);
+	mat(3,3) = at(0,0)*at(1,1)*at(2,2) + at(1,0)*at(2,1)*at(0,2) + at(2,0)*at(0,1)*at(1,2)
+				- at(0,0)*at(2,1)*at(1,2) - at(1,0)*at(0,1)*at(2,2) - at(2,0)*at(1,1)*at(0,2);
 
-    result[4] = IDX(1,2)*IDX(3,4)*IDX(4,3) + IDX(1,3)*IDX(3,2)*IDX(4,4) + IDX(1,4)*IDX(3,3)*IDX(4,2)
-              - IDX(1,2)*IDX(3,3)*IDX(4,4) - IDX(1,3)*IDX(3,4)*IDX(4,2) - IDX(1,4)*IDX(3,2)*IDX(4,3);
-    result[5] = IDX(1,1)*IDX(3,3)*IDX(4,4) + IDX(1,3)*IDX(3,4)*IDX(4,1) + IDX(1,4)*IDX(3,1)*IDX(4,3)
-              - IDX(1,1)*IDX(3,4)*IDX(4,3) - IDX(1,3)*IDX(3,1)*IDX(4,4) - IDX(1,4)*IDX(3,3)*IDX(4,1);
-    result[6] = IDX(1,1)*IDX(3,4)*IDX(4,2) + IDX(1,2)*IDX(3,1)*IDX(4,4) + IDX(1,4)*IDX(3,2)*IDX(4,1)
-              - IDX(1,1)*IDX(3,2)*IDX(4,4) - IDX(1,2)*IDX(3,4)*IDX(4,1) - IDX(1,4)*IDX(3,1)*IDX(4,2);
-    result[7] = IDX(1,1)*IDX(3,2)*IDX(4,3) + IDX(1,2)*IDX(3,3)*IDX(4,1) + IDX(1,3)*IDX(3,1)*IDX(4,2)
-              - IDX(1,1)*IDX(2,2)*IDX(4,3) - IDX(1,2)*IDX(2,3)*IDX(4,1) - IDX(1,3)*IDX(2,1)*IDX(4,2);
-
-    result[8] = IDX(1,2)*IDX(2,3)*IDX(4,4) + IDX(1,3)*IDX(2,4)*IDX(4,2) + IDX(1,4)*IDX(2,2)*IDX(4,3)
-              - IDX(1,2)*IDX(2,4)*IDX(4,3) - IDX(1,3)*IDX(2,2)*IDX(4,4) - IDX(1,4)*IDX(2,3)*IDX(4,2);
-    result[9] = IDX(1,1)*IDX(2,4)*IDX(4,3) + IDX(1,3)*IDX(2,1)*IDX(4,4) + IDX(1,4)*IDX(2,3)*IDX(4,1)
-              - IDX(1,1)*IDX(2,3)*IDX(4,4) - IDX(1,3)*IDX(2,4)*IDX(4,1) - IDX(1,4)*IDX(2,1)*IDX(4,3);
-    result[10] = IDX(1,1)*IDX(2,2)*IDX(4,4) + IDX(1,2)*IDX(2,4)*IDX(4,1) + IDX(1,4)*IDX(2,1)*IDX(4,2)
-               - IDX(1,1)*IDX(2,4)*IDX(4,2) - IDX(1,2)*IDX(2,1)*IDX(4,4) - IDX(1,4)*IDX(2,2)*IDX(4,1);
-    result[11] = IDX(1,1)*IDX(2,3)*IDX(4,2) + IDX(1,2)*IDX(2,1)*IDX(4,3) + IDX(1,3)*IDX(2,2)*IDX(4,1)
-               - IDX(1,1)*IDX(2,2)*IDX(4,3) - IDX(1,2)*IDX(2,3)*IDX(4,1) - IDX(1,3)*IDX(2,1)*IDX(4,2);
-
-    result[12] = IDX(1,2)*IDX(2,4)*IDX(3,3) + IDX(1,3)*IDX(2,2)*IDX(3,4) + IDX(1,4)*IDX(2,3)*IDX(3,2)
-               - IDX(1,2)*IDX(2,3)*IDX(3,4) - IDX(1,3)*IDX(2,4)*IDX(3,2) - IDX(1,4)*IDX(2,2)*IDX(3,3);
-    result[13] = IDX(1,1)*IDX(2,3)*IDX(3,4) + IDX(1,3)*IDX(2,4)*IDX(3,1) + IDX(1,4)*IDX(2,1)*IDX(3,3)
-               - IDX(1,1)*IDX(2,4)*IDX(3,3) - IDX(1,3)*IDX(2,1)*IDX(3,4) - IDX(1,4)*IDX(2,3)*IDX(3,1);
-    result[14] = IDX(1,1)*IDX(2,4)*IDX(3,2) + IDX(1,2)*IDX(2,1)*IDX(3,4) + IDX(1,4)*IDX(2,2)*IDX(3,1)
-               - IDX(1,1)*IDX(2,2)*IDX(3,4) - IDX(1,2)*IDX(2,4)*IDX(3,1) - IDX(1,4)*IDX(2,1)*IDX(3,2);
-    result[15] = IDX(1,1)*IDX(2,2)*IDX(3,3) + IDX(1,2)*IDX(2,3)*IDX(3,1) + IDX(1,3)*IDX(2,1)*IDX(3,2)
-               - IDX(1,1)*IDX(2,3)*IDX(3,2) - IDX(1,2)*IDX(2,1)*IDX(3,3) - IDX(1,3)*IDX(2,2)*IDX(3,1);
-
-    Matrix4 resultMat(result);
-    resultMat*=(1.f/det());
-
-    return resultMat;
+	return mat.transpose() * (1.f/det());
 }
 
 //--------------------------------------------------------------
 Matrix4 Matrix4::perspectiveProj(float fovx, float whRatio, float nearValue, float farValue)
 {
-    fovx *= 3.14159f/180.f;
-    fovx /= 2.f;
+	fovx *= 3.14159f/180.f;
+	fovx /= 2.f;
 
-    const float projX = 1.f/(tanf(fovx));
-    const float projY = projX * whRatio;
-    const float A = - (farValue+nearValue) / (farValue-nearValue);
-    const float B = -2.f * (farValue*nearValue) / (farValue-nearValue);
+	const float projX = 1.f/(tanf(fovx));
+	const float projY = projX * whRatio;
+	const float A = - (farValue+nearValue) / (farValue-nearValue);
+	const float B = -2.f * (farValue*nearValue) / (farValue-nearValue);
 
-    const float data[16] = {
-        projX,         0.f,           0.f,          0.f,
-        0.f,           projY,         0.f,          0.f,
-        0.f,           0.f,           A,            B,
-        0.f,           0.f,           -1.f,         0.f
-    };
+	const float data[16] = {
+		projX,         0.f,           0.f,          0.f,
+		0.f,           projY,         0.f,          0.f,
+		0.f,           0.f,           A,            B,
+		0.f,           0.f,           -1.f,         0.f
+	};
 
-    return Matrix4(data, true);
+	return Matrix4(data, true);
 }
 
 //--------------------------------------------------------------
 Matrix4 Matrix4::orthographicProj(float l, float r, float b, float t, float nearValue, float farValue)
 {
-    const float projX = 2.f/(r-l);
-    const float projY = 2.f/(t-b);
-    const float projZ = -2/(farValue-nearValue);
-    const float tX = -(r+l)/(r-l);
-    const float tY = -(t+b)/(t-b);
-    const float tZ = -(farValue+nearValue)/(farValue-nearValue);
+	const float projX = 2.f/(r-l);
+	const float projY = 2.f/(t-b);
+	const float projZ = -2/(farValue-nearValue);
+	const float tX = -(r+l)/(r-l);
+	const float tY = -(t+b)/(t-b);
+	const float tZ = -(farValue+nearValue)/(farValue-nearValue);
 
-    const float data[16] = {
-        projX,    0.f,      0.f,        tX,
-        0.f,      projY,    0.f,        tY,
-        0.f,      0.f,      projZ,      tZ,
-        0.f,      0.f,      0.f,        1.f
-    };
+	const float data[16] = {
+		projX,    0.f,      0.f,        tX,
+		0.f,      projY,    0.f,        tY,
+		0.f,      0.f,      projZ,      tZ,
+		0.f,      0.f,      0.f,        1.f
+	};
 
-    return Matrix4(data, true);
+	return Matrix4(data, true);
 }
 
 //--------------------------------------------------------------
 Matrix4 Matrix4::orthographicProj(float width, float height, float nearValue, float farValue)
 {
-    const float projX = 2.f/width;
-    const float projY = 2.f/height;
-    const float projZ = -2/(farValue-nearValue);
-    const float tZ = -(farValue+nearValue)/(farValue-nearValue);
+	const float projX = 2.f/width;
+	const float projY = 2.f/height;
+	const float projZ = -2/(farValue-nearValue);
+	const float tZ = -(farValue+nearValue)/(farValue-nearValue);
 
-    const float data[16] = {
-        projX,    0.f,      0.f,        0.f,
-        0.f,      projY,    0.f,        0.f,
-        0.f,      0.f,      projZ,      tZ,
-        0.f,      0.f,      0.f,        1.f
-    };
+	const float data[16] = {
+		projX,    0.f,      0.f,        0.f,
+		0.f,      projY,    0.f,        0.f,
+		0.f,      0.f,      projZ,      tZ,
+		0.f,      0.f,      0.f,        1.f
+	};
 
-    return Matrix4(data, true);
+	return Matrix4(data, true);
 }
 
 //--------------------------------------------------------------
 Matrix4 Matrix4::view(const Vec3& pos, const Vec3& target, const Vec3& up)
 {
-    Vec3 zAxis = pos-target; zAxis.normalize();
-    Vec3 xAxis = up.cross(zAxis); xAxis.normalize();
-    Vec3 yAxis = zAxis.cross(xAxis);
+	Vec3 zAxis = pos-target; zAxis.normalize();
+	Vec3 xAxis = up.cross(zAxis); xAxis.normalize();
+	Vec3 yAxis = zAxis.cross(xAxis);
 
-    const float data[16] = {
-        xAxis.x(),                   xAxis.y(),                     xAxis.z(),                    -xAxis.dot(pos),
-        yAxis.x(),                   yAxis.y(),                     yAxis.z(),                    -yAxis.dot(pos),
-        zAxis.x(),                   zAxis.y(),                     zAxis.z(),                    -zAxis.dot(pos),
-        0.0,                         0.0,                           0.0,                          1.f
-    };
+	const float data[16] = {
+		xAxis.x(),    xAxis.y(),    xAxis.z(),    -xAxis.dot(pos),
+		yAxis.x(),    yAxis.y(),    yAxis.z(),    -yAxis.dot(pos),
+		zAxis.x(),    zAxis.y(),    zAxis.z(),    -zAxis.dot(pos),
+		0.0,          0.0,          0.0,          1.f
+	};
 
-    return Matrix4(data,true);
+	return Matrix4(data,true);
 }
 
 //--------------------------------------------------------------
 Matrix4 Matrix4::translation(float tx, float ty, float tz)
 {
-    const float data[16] = {
-        1.f,    0.f,    0.f,    tx,
-        0.f,    1.f,    0.f,    ty,
-        0.f,    0.f,    1.f,    tz,
-        0.f,    0.f,    0.f,    1.f
-    };
+	const float data[16] = {
+		1.f,    0.f,    0.f,    tx,
+		0.f,    1.f,    0.f,    ty,
+		0.f,    0.f,    1.f,    tz,
+		0.f,    0.f,    0.f,    1.f
+	};
 
-    return Matrix4(data, true);
+	return Matrix4(data, true);
 }
 
 //--------------------------------------------------------------
 Matrix4 Matrix4::rotationX(float rx)
 {
-    const float data[16] = {
-        1.f,    0.f,        0.f,        0.f,
-        0.f,    std::cos(rx),   -std::sin(rx),  0.f,
-        0.f,    std::sin(rx),   std::cos(rx),   0.f,
-        0.f,    0.f,        0.f,        1.f
-    };
+	float c = std::cos(rx);
+	float s = std::sin(rx);
 
-    return Matrix4(data, true);
+	const float data[16] = {
+		1.f,    0.f,    0.f,    0.f,
+		0.f,    c,      -s,     0.f,
+		0.f,    s,      c,      0.f,
+		0.f,    0.f,    0.f,    1.f
+	};
+
+	return Matrix4(data, true);
 }
 
 
 //--------------------------------------------------------------
 Matrix4 Matrix4::rotationY(float ry)
 {
-    const float data[16] = {
-        std::cos(ry),    0.f,        std::sin(ry),   0.f,
-        0.f,         1.f,        0.f,        0.f,
-        -std::sin(ry),   0.f,        std::cos(ry),   0.f,
-        0.f,         0.f,        0.f,        1.f
-    };
+	float c = std::cos(ry);
+	float s = std::sin(ry);
 
-    return Matrix4(data, true);
+	const float data[16] = {
+		c,      0.f,    s,      0.f,
+		0.f,    1.f,    0.f,    0.f,
+		-s,     0.f,    c,      0.f,
+		0.f,    0.f,    0.f,    1.f
+	};
+
+	return Matrix4(data, true);
 }
 
 
 //--------------------------------------------------------------
 Matrix4 Matrix4::rotationZ(float rz)
 {
-    const float data[16] = {
-        std::cos(rz),    -std::sin(rz),   0.f,    0.f,
-        std::sin(rz),    std::cos(rz),    0.f,    0.f,
-        0.f,        0.f,          1.f,    0.f,
-        0.f,        0.f,          0.f,    1.f
-    };
+	float c = std::cos(rz);
+	float s = std::sin(rz);
 
-    return Matrix4(data, true);
+	const float data[16] = {
+		c,      -s,     0.f,    0.f,
+		s,      c,      0.f,    0.f,
+		0.f,    0.f,    1.f,    0.f,
+		0.f,    0.f,    0.f,    1.f
+	};
+
+	return Matrix4(data, true);
 }
 
 //--------------------------------------------------------------
 Matrix4 Matrix4::scale(float sx, float sy, float sz)
 {
-    const float data[16] = {
-        sx,     0.f,    0.f,    0.f,
-        0.f,    sy,     0.f,    0.f,
-        0.f,    0.f,    sz,     0.f,
-        0.f,    0.f,    0.f,    1.f
-    };
+	const float data[16] = {
+		sx,     0.f,    0.f,    0.f,
+		0.f,    sy,     0.f,    0.f,
+		0.f,    0.f,    sz,     0.f,
+		0.f,    0.f,    0.f,    1.f
+	};
 
-    return Matrix4(data);
+	return Matrix4(data);
 }
 
 //--------------------------------------------------------------

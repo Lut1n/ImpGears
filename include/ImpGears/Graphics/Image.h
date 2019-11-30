58d6cbdaacfa862f0a08e01d52ac1a1047e91c7d
Lut1n <mathieu.boulet.44@gmail.com>
2019-11-30 11:37:11 +0100

    auto-indent on Image.h

--- include/ImpGears/Graphics/Image.h
+++ include/ImpGears/Graphics/Image.h
@@ -10,7 +10,7 @@ IMPGEARS_BEGIN
 
 class IMP_API Image : public Object
 {
- 
+
 public:
     
     using b8 = std::uint8_t;
@@ -20,26 +20,26 @@ public:
     Image(int w, int h, int chnls);
     ~Image();
     
-	void copy(const Image::Ptr& other);
-	void copy(const Image::Ptr& other, const std::vector<int>& mask);
-	
+    void copy(const Image::Ptr& other);
+    void copy(const Image::Ptr& other, const std::vector<int>& mask);
+
     void setPixel(int x, int y, const Vec4& col);
     Vec4 getPixel(int x, int y) const;
-	
+
     void setPixel(const Vec2& uv, const Vec4& col);
     Vec4 getPixel(const Vec2& uv) const;
-	
+
     void fill(const Vec4& col);
-	
-	void resize(int w, int h, int chnls);
+
+    void resize(int w, int h, int chnls);
     
     int height() const;
     int width() const;
     int channels() const;
     
     const Grid<3,b8>::Ptr asGrid() const;
-	const b8* data() const;
-	b8* data();
+    const b8* data() const;
+    b8* data();
     
 private:
     

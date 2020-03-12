13aea62c4dc0908526c0c3ea92d8d5573a397a19
Lut1n <mathieu.boulet.44@gmail.com>
2020-03-12 18:39:32 +0100

    fix indentation

--- include/ImpGears/Core/Object.h
+++ include/ImpGears/Core/Object.h
@@ -14,29 +14,29 @@
 #include <Core/Config.h>
 
 #define Meta_Class(name) \
-	typedef std::shared_ptr<name> Ptr;\
+    typedef std::shared_ptr<name> Ptr;\
     static name::Ptr dMorph(Object::Ptr obj){return std::dynamic_pointer_cast<name>(obj);}\
     static name::Ptr sMorph(Object::Ptr obj){return std::static_pointer_cast<name>(obj);}\
-	static std::string getClassName() {return std::string( #name );}\
-	virtual std::string getObjectID() const {std::stringstream ss; ss << getClassName() << "@" << (this); return ss.str();}\
-	template<class... Args> static Ptr create(Args&&... args) {return std::make_shared<name>(std::forward<Args>(args)...);}
+    static std::string getClassName() {return std::string( #name );}\
+    virtual std::string getObjectID() const {std::stringstream ss; ss << getClassName() << "@" << (this); return ss.str();}\
+    template<class... Args> static Ptr create(Args&&... args) {return std::make_shared<name>(std::forward<Args>(args)...);}
 
 IMPGEARS_BEGIN
 	
 class IMP_API Object
 {
-	public:
-	
-	Meta_Class(Object)
-	
-	Object(){}
-	virtual ~Object(){}
-    
+    public:
+
+    Meta_Class(Object)
+
+    Object(){}
+    virtual ~Object(){}
+
     Object::Ptr getRenderData(){return _renderData;}
     void setRenderData(Object::Ptr obj){_renderData=obj;}
-    
+
     private:
-    
+
     Object::Ptr _renderData;
 };
 
--- include/ImpGears/SceneGraph/RenderToSamplerNode.h
+++ include/ImpGears/SceneGraph/RenderToSamplerNode.h
@@ -18,27 +18,24 @@ public:
 
     CubeMapSampler::Ptr cubemap() {return _cubemap;}
     void setCubeMap(CubeMapSampler::Ptr cm){_cubemap=cm;}
-    
+
     ImageSampler::Ptr texture() {return _texture;}
     void setTexture(ImageSampler::Ptr tx){_texture=tx;}
-    
+
     Graph::Ptr scene() {return _scene;}
     void setScene(Graph::Ptr s){_scene=s;}
-    
+
     bool ready() const {return _rendered;}
     void makeReady() {_rendered=true;}
     void makeDirty() {_rendered=false;}
-    
 
 protected:
 
     CubeMapSampler::Ptr _cubemap;
     ImageSampler::Ptr _texture;
-    
+
     Graph::Ptr _scene;
     bool _rendered;
-
-    
 };
 
 IMPGEARS_END
